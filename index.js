c8cbda6a740b82c44d8aea269aa7328fc2331b7c
EmanuelPatricio <emanuelptr81@gmail.com>
2021-12-08 16:44:41 -0400

    toKelvin() method created and code refractoring

--- index.js
+++ index.js
@@ -9,6 +9,24 @@ module.exports = class Temperature {
   toFahrenheit() {
     if (this.scale === 'f'.toUpperCase()) throw new Error('Can not convert a temperature to it\'s same scale');
 
-    return this.value * 9/5 + 32;
+    const temperatureConverter = () => {
+      if (this.scale === 'k'.toUpperCase()) {
+        return Number(((this.value - 273.15) * 9/5 + 32).toFixed(3));
+      }
+      return Number((this.value * 9/5 + 32).toFixed(3));
+    }
+    return new Temperature(temperatureConverter(), 'F');
+  }
+
+  toKelvin() {
+    if (this.scale === 'k'.toUpperCase()) throw new Error('Can not convert a temperature to it\'s same scale');
+
+    const temperatureConverter = () => {
+      if (this.scale === 'f'.toUpperCase()) {
+        return Number(((this.value - 32) * 5/9 + 273.15).toFixed(3));
+      }
+      return Number((this.value + 273.15).toFixed(3));
+    }
+    return new Temperature(temperatureConverter(), 'K');
   }
 }
--- index.test.js
+++ index.test.js
@@ -2,9 +2,13 @@ const { it, expect } = require('@jest/globals');
 const Temperature = require('./index');
 
 describe('Temperature', () => {
-  const temperature = new Temperature(5.48, 'C');
+  const temperature = new Temperature(5.48, 'c');
 
-  it('toFahrenheit() must return a temperature converted to Fahrenheit', () => {
-    expect(temperature.toFahrenheit()).toBe(41.864000000000004);
+  it('toFahrenheit() must return a new temperature converted to Fahrenheit', () => {
+    expect(temperature.toFahrenheit()).toEqual({"value": 41.864, "scale": 'F'});
+  });
+
+  it('toKelvin() must return a temperature converted to Kelvin', () => {
+    expect(temperature.toKelvin()).toEqual({"value": 278.63, "scale": 'K'});
   });
 });
